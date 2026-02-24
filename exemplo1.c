a3f3ed0d65ce18a6a9b791d5933b0b72c5849fd6
Thiago Azevedo <160196041+thiago678@users.noreply.github.com>
2026-02-24 20:38:00 +0000

    committed com o ano

--- exemplo1.c
+++ exemplo1.c
@@ -2,8 +2,9 @@
 
 int main(){
 printf("Mackenzie - FCI\n");
-printf("Algoritimos e\n");
-printf("Programação II\n");
+printf("Algoritimos\n");
+printf("E Programação II\n");
+printf("FCI foi fundada em 1971\n");
 
 return 0;
 }
